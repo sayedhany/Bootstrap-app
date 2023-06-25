55426eb1e5de2365fd8ec910d417162f6c2705bc
sayedhany <sayedh@mail.ru>
2023-06-25 22:50:39 +0300

    add more sections

--- js/plugins.js
+++ js/plugins.js
@@ -1,10 +1,13 @@
+const form = document.querySelector(".form-inline");
+const currentYearElement = document.getElementById("current-year");
+let currentYear = new Date().getFullYear();
 $(document).ready(function () {
   $(".carousel").carousel({
     interval: 5000,
   });
 });
 
-const form = document.querySelector(".form-inline");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
+currentYearElement.textContent = currentYear;
