affeadf239c513f9a5664e7e8abdc2fb7accdebe
sayedhany <sayedh@mail.ru>
2023-06-27 17:40:39 +0300

    add more thems

--- js/plugins.js
+++ js/plugins.js
@@ -1,13 +1,12 @@
-const form = document.querySelector(".form-inline");
 const currentYearElement = document.getElementById("current-year");
+const gearBox = document.querySelector(".option-box .color-option");
 let currentYear = new Date().getFullYear();
+currentYearElement.textContent = currentYear;
 $(document).ready(function () {
   $(".carousel").carousel({
     interval: 5000,
   });
+  $(".gear-check").click(() => {
+    $(".color-option").fadeToggle();
+  });
 });
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-currentYearElement.textContent = currentYear;
