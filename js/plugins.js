0f8f909138567d3e9309ab19ddee19991ed3c601
sayedhany <sayedh@mail.ru>
2023-06-29 18:29:16 +0300

    add loading spinner

--- js/plugins.js
+++ js/plugins.js
@@ -9,4 +9,31 @@ $(document).ready(function () {
   $(".gear-check").click(() => {
     $(".color-option").fadeToggle();
   });
+  var colorLi = $(".color-option ul li");
+  colorLi.eq(0).css("backgroundColor", "#E41B17").end();
+  colorLi.eq(1).css("backgroundColor", "#e426d5").end();
+  colorLi.eq(2).css("backgroundColor", "#009aff").end();
+  colorLi.eq(3).css("backgroundColor", "#ffd500");
+  colorLi.click(function () {
+    $("link[href*='theme']").attr("href", $(this).attr("data-value"));
+    // console.log($(this).attr("data-value"));
+  });
+});
+
+$(window).load(function () {
+  "use strict";
+
+  // Loading Elements
+
+  $(".loading-overlay .spinner").fadeOut(2000, function () {
+    // Show The Scroll
+
+    $("body").css("overflow", "auto");
+
+    $(this)
+      .parent()
+      .fadeOut(2000, function () {
+        $(this).remove();
+      });
+  });
 });
