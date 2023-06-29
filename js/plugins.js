4ece2adaa1d90b155d1349f2e140fe2b5e8a8460
sayedhany <sayedh@mail.ru>
2023-06-29 21:35:18 +0300

    add some animation

--- js/plugins.js
+++ js/plugins.js
@@ -3,13 +3,15 @@ const gearBox = document.querySelector(".option-box .color-option");
 let currentYear = new Date().getFullYear();
 currentYearElement.textContent = currentYear;
 $(document).ready(function () {
+  // $("html").niceScroll();
   $(".carousel").carousel({
     interval: 5000,
   });
   $(".gear-check").click(() => {
     $(".color-option").fadeToggle();
   });
-  var colorLi = $(".color-option ul li");
+  var colorLi = $(".color-option ul li"),
+    scrollButton = $("#scroll-top");
   colorLi.eq(0).css("backgroundColor", "#E41B17").end();
   colorLi.eq(1).css("backgroundColor", "#e426d5").end();
   colorLi.eq(2).css("backgroundColor", "#009aff").end();
@@ -17,6 +19,21 @@ $(document).ready(function () {
   colorLi.click(function () {
     $("link[href*='theme']").attr("href", $(this).attr("data-value"));
     // console.log($(this).attr("data-value"));
+  });
+  // Caching The Scroll Top Element
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 700) {
+      scrollButton.show();
+    } else {
+      scrollButton.hide();
+    }
+  });
+
+  // Click On Button To Scroll Top
+
+  scrollButton.click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 800);
   });
 });
 
